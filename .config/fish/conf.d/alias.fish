001bd0d158beb0b4ca12b226b72fd8a427871366
Nikolas Bushery <118590190+nixbush@users.noreply.github.com>
2023-08-15 19:01:02 +0800

    Restructured alias.fish

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -1,11 +1,15 @@
+# Core Programs
 alias vi '$EDITOR'
-alias ls '/usr/bin/ls -lhAv --group-directories-first --color=auto'
+
 alias mkdir '/usr/bin/mkdir -p'
+alias ls '/usr/bin/ls -lhAv --group-directories-first --color=auto'
 alias rm '/usr/bin/trash-put'
-alias purge '/usr/bin/rm -r'
 alias cp '/usr/bin/cp -r'
-alias hibernate '/usr/bin/systemctl hibernate'
+
 alias tree '/usr/bin/tree -aCL 3 --gitignore --dirsfirst'
-alias dotfiles '/usr/bin/git --git-dir=$HOME/.repos/dotfiles --work-tree=$HOME'
-alias cbclr '/usr/bin/wl-copy -c; wl-copy -cp' # clear clipboard
+
+# Custom
+alias purge '/usr/bin/rm -r'
+alias hibernate '/usr/bin/systemctl hibernate'
 alias vg '/usr/bin/valgrind --leak-check=full --show-reachable=no --keep-debuginfo=yes --track-origins=yes'
+alias dotfiles '/usr/bin/git --git-dir=$HOME/.repos/dotfiles --work-tree=$HOME'
